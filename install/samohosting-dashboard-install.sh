0b1fe4544e36a944afe5f056b23a4005f667dc3f
samohosting <email@samohosting.ru>
2025-01-12 15:18:57 +0300

    Create samohosting-dashboard-install.sh

---
+++ install/samohosting-dashboard-install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://raw.githubusercontent.com/samohosting-ru/samohosting-scripts/ru_dev/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Устанавливаю зависимости(необходимое ПО).."
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y gpg
+msg_ok "Зависимости(необходимое ПО) установлены."
+
+msg_info "Настраиваю Node.js Репозиторий"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Репозиторий Node.js настроен"
+
+msg_info "Устанавливаю Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
+msg_ok "Node.js установлен"
+
+RELEASE=$(curl -s https://api.github.com/repos/Lissy93/dashy/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+msg_info "Устанавливаю Samohosting-Dashy-Start-Dashboard ${RELEASE} (Patience)"
+mkdir -p /opt/dashy
+wget -qO- https://github.com/Lissy93/dashy/archive/refs/tags/${RELEASE}.tar.gz | tar -xz -C /opt/dashy --strip-components=1
+cd /opt/dashy
+$STD npm install
+$STD npm run build
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
+msg_ok "Installed Dashy ${RELEASE}"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/dashy.service
+[Unit]
+Description=dashy
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/dashy
+ExecStart=/usr/bin/npm start
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl -q --now enable dashy
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Временные файлы установки - удалены!"
