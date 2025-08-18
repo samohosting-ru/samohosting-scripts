8e05d5fe55444a43d8c5e5a25f96a528432ea5bc
samohosting <email@samohosting.ru>
2025-08-18 23:17:32 +0300

    Update start-samohosting-install.sh

--- install/start-samohosting-install.sh
+++ install/start-samohosting-install.sh
@@ -539,9 +539,13 @@ msg_ok "Ваш новенький домашний сервер собран и 
 echo -e "     ${BOLD}${BL}Начните изучать Ваш домашний сервер by samohosting.ru${CL} ==>> ${BGN}http://${IP}:1000${CL} Удачного самохостинга!"
 echo -e "${TAB}${HOSTNAME}${BL} Начните изучать Ваш домашний сервер by samohosting.ru${CL} ==>> ${BGN}http://${IP}:1000${CL} Удачного самохостинга!" >> /etc/motd
 echo -e "       *если runtipi на дашборде не горит зеленым :"
+echo -e "       0. откройте shell(консоль) контейнера"
 echo -e "       1. перейдите в каталог runtipi: cd /opt/runtipi/"
 echo -e "       2. выполните команду: ./runtipi-cli start"
 echo -e "       *если runtipi на дашборде не горит зеленым :" >> /etc/motd
+echo -e "       https://forum.samohosting.ru/t/o-sborke-zachem-ona-na-chem-osnovana-chto-daet/30/7?u=admin"
+echo -e "       0. откройте shell(консоль) контейнера" >> /etc/motd
 echo -e "       1. перейдите в каталог runtipi: cd /opt/runtipi/" >> /etc/motd
 echo -e "       2. выполните команду: ./runtipi-cli start" >> /etc/motd
+echo -e "       https://forum.samohosting.ru/t/o-sborke-zachem-ona-na-chem-osnovana-chto-daet/30/7?u=admin" >> /etc/motd
 
